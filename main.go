21d6610efcf23f92541efbb9dcf5e1f2e2ad35bd
Amir Alesheikh <amirhossein.alesheikh@gmail.com>
2022-05-19 17:22:08 +0430

    feat(flags): improved organization of arguments

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
-var prFlags []cli.Flag = []cli.Flag{
+var printFlags []cli.Flag = []cli.Flag{
 	&cli.StringFlag{
 		Name:    "colorname",
 		Aliases: []string{"c"},
@@ -30,6 +30,34 @@ var prFlags []cli.Flag = []cli.Flag{
 		Value:   "rosePine",
 		Usage:   "Palette to use.(not developed for now)",
 	},
+	&cli.BoolFlag{
+		Name:    "all",
+		Aliases: []string{"a"},
+		Usage:   "Palette to use.",
+	},
+}
+var cpFlags []cli.Flag = []cli.Flag{
+	&cli.StringFlag{
+		Name:     "colorname",
+		Aliases:  []string{"c"},
+		Value:    "",
+		Usage:    "Color name.",
+		Required: true,
+	},
+	&cli.StringFlag{
+		Name:    "swatch",
+		Aliases: []string{"s"},
+		Value:   "rp",
+		Usage:   "Swatch to use.",
+		EnvVars: []string{"DEFAULTSWATCH"},
+	},
+	&cli.StringFlag{
+		Name:    "palette",
+		Aliases: []string{"p"},
+		Value:   "rosePine",
+		Usage:   "Palette to use. (not developed for now)",
+		EnvVars: []string{"DEFAULTPALLETE"},
+	},
 }
 
 func main() {
@@ -39,25 +67,18 @@ func main() {
 		Usage: "Your friendly rosepine helper.",
 		Commands: []*cli.Command{
 			{
-				Name:    "pr",
+				Name:    "print",
 				Aliases: []string{"p"},
 				Usage:   "Print out the color code of the given string",
-				Flags:   prFlags,
+				Flags:   printFlags,
 				Action:  printer,
 			},
 			{
 				Name:    "copy",
-				Aliases: []string{"c"},
+				Aliases: []string{"cp"},
 				Usage:   "Store code inside clipboard",
-				Flags:   prFlags,
+				Flags:   cpFlags,
 				Action:  cp,
-			},
-			{
-				Name:    "print-all",
-				Aliases: []string{"a"},
-				Usage:   "print all the codes of a colorscheme",
-				Flags:   prFlags,
-				Action:  printAll,
 			},
 		},
 	}
@@ -69,6 +90,18 @@ func main() {
 
 }
 func printer(c *cli.Context) error {
+	if c.Bool("all") {
+		swatch := match.List(c.String("swatch"))
+		for n, ing := range swatch {
+			col := ing.Hex
+			block := fmt.Sprint(lipgloss.NewStyle().Background(lipgloss.Color(col)).PaddingRight(2).Render(""))
+			code := fmt.Sprint(lipgloss.NewStyle().Bold(true).Italic(true).Render(block + "\t" + col + "\t\t" + n))
+			fmt.Println(code)
+		}
+		return nil
+
+	}
+
 	col := match.Get(c.String("colorname"), c.String("swatch"))
 	block := fmt.Sprint(lipgloss.NewStyle().Background(lipgloss.Color(col)).PaddingRight(2).Render(""))
 	code := fmt.Sprint(lipgloss.NewStyle().Bold(true).Italic(true).Render(block + " " + col))
@@ -83,13 +116,14 @@ func cp(c *cli.Context) error {
 	}
 	return nil
 }
-func printAll(c *cli.Context) error {
-	swatch := match.List(c.String("swatch"))
-	for n, ing := range swatch {
-		col := ing.Hex
-		block := fmt.Sprint(lipgloss.NewStyle().Background(lipgloss.Color(col)).PaddingRight(2).Render(""))
-		code := fmt.Sprint(lipgloss.NewStyle().Bold(true).Italic(true).Render(block + " " + col + " " + n))
-		fmt.Println(code)
-	}
-	return nil
-}
+
+// func printAll(c *cli.Context) error {
+// 	swatch := match.List(c.String("swatch"))
+// 	for n, ing := range swatch {
+// 		col := ing.Hex
+// 		block := fmt.Sprint(lipgloss.NewStyle().Background(lipgloss.Color(col)).PaddingRight(2).Render(""))
+// 		code := fmt.Sprint(lipgloss.NewStyle().Bold(true).Italic(true).Render(block + " " + col + " " + n))
+// 		fmt.Println(code)
+// 	}
+// 	return nil
+// }
